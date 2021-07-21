27d9f1421f17740f1bbbb9e62899c87656c48db5
Jaewon-Seo97 <55906297+Jaewon-Seo97@users.noreply.github.com>
2021-07-21 18:13:41 +0900

    Add files via upload

--- preprocessing_tool.py
+++ preprocessing_tool.py
@@ -2,6 +2,12 @@ import numpy as np
 import cv2
 
 def matrix2uint (matrix, in_bit = 16, out_bit= 8, out_dtype = np.uint8):
+    """
+    matrix : array
+    in_bit: bit of input array, dtype= int
+    out_bit: bit of output array, dtype= int
+    out_dtype: numpy dtype of output array, ex) np.uint8, np.uint16 ...
+    """
     m_min= 0
     m_max= 2**in_bit-1
     matrix=matrix-m_min
@@ -9,6 +15,9 @@ def matrix2uint (matrix, in_bit = 16, out_bit= 8, out_dtype = np.uint8):
     return output
 
 def resize_image(imageSize, in_arr, channel=False, in_out_channel_same=True, dtype = np.uint8):
+    """
+    imageSize, in_arr, channel=False, in_out_channel_same=True, dtype = np.uint8
+    """
     img_stand = max(in_arr.shape)
     img=in_arr
     scale = imageSize/img_stand
